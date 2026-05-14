440f029598847f038d05bcf118e03491bc37339d
Hassan Chehadi <e12119831@student.tuwien.ac.at>
2026-05-14 14:10:53 +0200

    adapting zoom factor

--- src/main.java
+++ src/main.java
@@ -11,7 +11,7 @@ public class main {
     public static double minusY = -1;
     public static double plusY = 1;
     public static int aufloesung = 1500;
-    public static int zoomfactor = 100;
+    public static int zoomfactor = 15;
     public static CodeDraw mydrawObj = new CodeDraw(1600, 800);
 
     public static void main(String[] args) {
